dd89ce3a4ecf5569a4555bc4797989705716eadc
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-11 09:48:24 -0300

    encapsulando a classe Moeda

--- src/cofrinho/Moeda.java
+++ src/cofrinho/Moeda.java
@@ -5,8 +5,23 @@ public abstract class Moeda {
 	private String nome;
 	private double valor;
 	
-	public void exibeInformacoes() {
-		System.out.println("Nome: " + nome);
-		System.out.println("Valor: " + valor);
+	public String getNome() {
+		return nome;
 	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public abstract void exibeInformacoes();
+	
+	public abstract double converter();
 }
---
+++ src/cofrinho/Real.java
@@ -0,0 +1,9 @@
+package cofrinho;
+
+public class Real extends Moeda {
+	@Override
+	public double converter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
