cf0162161ffc1c8a3c1547e64933574f3939a144
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-11 09:26:02 -0300

    método para exibir informações

--- src/cofrinho/Moeda.java
+++ src/cofrinho/Moeda.java
@@ -2,5 +2,11 @@ package cofrinho;
 
 public abstract class Moeda {
 
+	private String nome;
 	private double valor;
+	
+	public void exibeInformacoes() {
+		System.out.println("Nome: " + nome);
+		System.out.println("Valor: " + valor);
+	}
 }
