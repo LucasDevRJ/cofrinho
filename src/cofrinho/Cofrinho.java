40aecac76da6032655d47bf800918c444c994637
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-11 09:19:25 -0300

    método para listar moedas

--- src/cofrinho/Cofrinho.java
+++ src/cofrinho/Cofrinho.java
@@ -6,11 +6,17 @@ public class Cofrinho {
 
 	private ArrayList<Moeda> moedas = new ArrayList<Moeda>();
 	
-	public void adicionar(Moeda moeda) {
+	public void adicionarMoeda(Moeda moeda) {
 		this.moedas.add(moeda);
 	}
 	
-	public void remover(Moeda moeda) {
+	public void removerMoeda(Moeda moeda) {
 		this.moedas.remove(moeda);
+	}
+	
+	public void listagemMoedas() {
+		for (Moeda moeda : moedas) {
+			System.out.println(moeda);
+		}
 	}
 }
